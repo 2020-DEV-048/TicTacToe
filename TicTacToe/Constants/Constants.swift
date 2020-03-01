f20a5461789677cbe0e6923325fd7bd9a5fcb6c4
Dev2020 <dev2020.048@gmail.com>
2020-03-02 02:55:34 +0530

    Git Setup

---
+++ TicTacToe/Constants/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  TicTacToe
+//
+//  Created by 2020-DEV-048  on 3/2/20.
+//  Copyright © 2020 2020. All rights reserved.
+
+//  DESCRIPTION: This class will contain all the constants and common functions that are used in application
+
+import Foundation
---
+++ TicTacToe/ViewControllers/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  TicTacToe
+//
+//  Created by 2020-DEV-048 on 3/2/20.
+//  Copyright © 2020 2020. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
